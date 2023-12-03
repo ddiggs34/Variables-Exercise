66a08bb64780c1d08e1875ab98779684396a5694
David Diggs <ddiggs34@gmail.com>
2023-12-02 19:25:41 -0500

    done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string happy = "Im so happy";
+            int donuts = 12;
+            char middleInitial = 'M';
+            bool canSee = true;
+            double radioStation = 96.3;
+            decimal change = 18.01m;
+
+            Console.WriteLine($"{happy}, because I brought {donuts} donuts. My middle initial is {middleInitial}. My favorite radio station is {radioStation}. My vision is good. Yes {canSee}. I have ${change} left in my pocket");
+
         }
     }
 }
